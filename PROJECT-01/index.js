94f9649752838274d2d73a96432438dfdc0309d5
Rohit Guru <officialrohitguru@gmail.com>
2024-06-03 09:00:17 +0530

    till middleware

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json")
 const app = express();
 const PORT = 8000;
 
+// Middleware
+app.use(express.urlencoded({extended:false}));
+
+app.use((req,res,next)=>{
+    fs.appendFile(
+        "log.txt",`\n${Date.now()}:${req.ip} ${req.method}: ${req.path}`,
+        (err, data) => {
+            next();
+        }
+    )
+})
+app.use((req,res,next)=>{
+    console.log('Hello from middleware 1');
+    // return res.json({msg: "Hello from middleware 1"});
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('Hello from middleware 1');
+    // return res.json({msg: "Hello from middleware 1"});
+//    res.end('ending')
+   next();
+})
 //Routes
 
 app.get('/users', (req,res)=>{
@@ -19,7 +42,7 @@ app.get('/api/users',(req,res)=>{
     return res.json(users);
 })
 
-app.route('api/users/:id').get((req,res)=>{
+app.route('/api/users/:id').get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user)=> user.id === id);
     return res.json(user);
@@ -29,7 +52,7 @@ app.route('api/users/:id').get((req,res)=>{
 }).delete((req,res)=>{
     // TODO:Delete the user with id
     return res.json({status:"pending"})
-})
+});
 
 // app.get('/api/users/:id',(req,res)=>{
 //     const id = Number(req.params.id);
@@ -37,10 +60,14 @@ app.route('api/users/:id').get((req,res)=>{
 //     return res.json(user);
 // })
 
-app.post('api/users', (req,res)=>{
+app.post('/api/users', (req,res) => {
     // TODO: Create new user
-    return res.json({status:"pending"})
-})
+    const body = req.body;
+    users.push({...body, id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+       return res.json({status: "success", id: users.length}); 
+    })
+});
 // app.patch('api/users/:id', (req,res)=>{
 //     // TODO: Edit the user with id
 //     return res.json({status:"pending"})
